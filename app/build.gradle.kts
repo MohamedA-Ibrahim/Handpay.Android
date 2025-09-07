c9d8a0247d9a2725829b0014d47ce3d85e932146
Mohamed Ali <128579563+MohamedAliTera@users.noreply.github.com>
2025-09-08 00:43:42 +0300

    Modify Android min sdk

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,7 +9,7 @@ android {
 
     defaultConfig {
         applicationId = "io.handpay.app"
-        minSdk = 29
+        minSdk = 28
         targetSdk = 36
         versionCode = 1
         versionName = "1.0"
@@ -51,4 +51,5 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    implementation("io.nearpay:nearpay-sdk-store:2.1.94")
 }
